43b656c914d0d50d9b422e93723851a85b53d5d3
yanshifu <yanshifu2012@hotmail.com>
2020-06-22 11:42:39 +0900

    first

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(createMessage());
+		}
+		public static string createMessage()
+		{
+			return "Hello World";
+		}
+	}
+}
